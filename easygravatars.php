49f9318b90b08ecdd5a8b4bd60d23ced6f4b7bb9
dougal <dougal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-11-16 15:06:39 +0000

    Version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/easygravatars/trunk@24806 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- easygravatars.php
+++ easygravatars.php
@@ -3,23 +3,17 @@
  * Plugin Name: Easy Gravatars
  * Plugin URI: http://dougal.gunters.org/
  * Description: Add Gravatars to your comments, without requiring any modifications to your theme files. Just activate, and you're done!
- * Version: 1.0
+ * Version: 1.1
  * License: GPL2
  * Author: Dougal Campbell
  * Author URI: http://dougal.gunters.org/
  * Min WP Version: 2.0.4
- * Max WP Version: 2.3
+ * Max WP Version: 2.3.1
  */
 
 // Register our activation hook, so we can set our default options:
 register_activation_hook(_FILE_,'eg_activate');
 
-// Let WP know that we have an addition to the admin menus:
-add_action('admin_menu', 'eg_opt_menu');
-add_filter('get_comment_author_link', 'eg_gravatar');
-
-
-// On activation, set our default options:
 function eg_activate() {
 	// ...but only if we don't already have options set...
 	$eg_opt_size = get_option('eg_size');
@@ -31,6 +25,35 @@ function eg_activate() {
 	}
 }
 
+// Let WP know that we have an addition to the admin menus:
+add_action('admin_menu', 'eg_opt_menu');
+
+function eg_opt_menu() {
+	add_options_page('Easy Gravatars', 'Easy Gravatars', 'manage_options', 'testoptions', 'eg_options_page');
+}
+
+// Add in our custom CSS
+add_action('admin_head', 'eg_admin_css');
+
+function eg_admin_css() {
+        $plugindir = get_option('siteurl') . '/wp-content/plugins/' . dirname(plugin_basename(__FILE__));
+        $eg_css = $plugindir . '/easygravatars.css';
+?>
+<link rel="stylesheet" type="text/css" href="<?php echo $eg_css; ?>" />
+<?php
+
+}
+
+// Set the filter requested by the user:
+add_action('init', 'eg_set_filter');
+
+function eg_set_filter() {
+	$opts = eg_get_options();
+	$filter = $opts['eg_api_hook'];
+	add_filter($filter, 'eg_gravatar');
+}
+
+// Returns a hash of the default option values
 function eg_defaults() {
 	return array(
 		'eg_size' => 80,
@@ -38,6 +61,7 @@ function eg_defaults() {
 		'eg_defaulturl' => 'http://use.perl.org/images/pix.gif',
 		'eg_style_span' => 'float:right; margin-left:10px; display:block',
 		'eg_style_img' => '',
+                'eg_api_hook' => 'get_comment_author_link',
 	);
 }
 
@@ -48,6 +72,7 @@ function eg_get_options() {
 	$eg_defaulturl = get_option('eg_defaulturl');
 	$eg_style_span = get_option('eg_style_span');
 	$eg_style_img = get_option('eg_style_img');
+	$eg_api_hook = get_option('eg_api_hook');
 	
 	$defaults = eg_defaults();
 	// Extra paranoia:
@@ -61,6 +86,8 @@ function eg_get_options() {
 		$eg_style_span = $defaults['eg_style_span'];
 	if(empty($eg_style_img))
 		$eg_style_img = $defaults['eg_style_img'];
+        if(empty($eg_api_hook))
+                $eg_api_hook = $defaults['eg_api_hook'];
 		
 	return array(
 		'eg_size' => $eg_size,
@@ -68,34 +95,41 @@ function eg_get_options() {
 		'eg_defaulturl' => $eg_defaulturl,
 		'eg_style_span' => $eg_style_span,
 		'eg_style_img' => $eg_style_img,
+		'eg_api_hook' => $eg_api_hook,
 	);
 }
 
-// Filter to add a Gravatar image:
+// Filter to add a Gravatar image, prepended:
 function eg_gravatar($text) {
 	global $comment;
+	// no email? no gravatar.
 	if ( !empty( $comment->comment_author_email ) ) {
 		$opts = eg_get_options();
+
 	        // The Gravatar server normalizes email addresses to
 	        // lowercase, so we should, too. Props to David Potter:
 	        //   http://dpotter.net/Technical/index.php/2007/10/22/integrating-gravatar-support/
+
 		$md5 = md5( strtolower( $comment->comment_author_email ) );
-		// Need to break the default image and the inline style out 
-		// into config options.
+
 		$eg_size = $opts['eg_size'];
 		$eg_rating = $opts['eg_rating'];
 		$default = urlencode( $opts['eg_defaulturl'] );
 		$eg_style_span = $opts['eg_style_span'];
-		$text .= "<span class='eg-image'  style='$eg_style_span; width:{$eg_size}px' ><img src='http://www.gravatar.com/avatar.php?gravatar_id=$md5&amp;size=$eg_size&amp;rating=$eg_rating&amp;default=$default' width='$eg_size' height='$eg_size' alt='' /></span>";
+
+		$imgtag = "<img src='http://www.gravatar.com/avatar.php?gravatar_id=$md5&amp;size=$eg_size&amp;rating=$eg_rating&amp;default=$default' width='$eg_size' height='$eg_size' alt='' />";
+
+		$cau = $comment->comment_author_url;
+		// Did they leave a link?
+		if (! ( empty($cau) || 'http://' == $cau ) ) {
+			$imgtag = "<a rel='external nofollow' href='$cau'>$imgtag</a>";
+		}
+
+
+		$text = "<span class='eg-image' style='$eg_style_span; width:{$eg_size}px' >$imgtag</span>" . $text;
 	}
 
 	return $text;
-}
-
-
-// Hook our config page into the Options menu:
-function eg_opt_menu() {
-	add_options_page('Easy Gravatars', 'Easy Gravatars', 'manage_options', 'testoptions', 'eg_options_page');
 }
 
 // Options update page:
@@ -104,6 +138,10 @@ function eg_options_page() {
 	$eg_ratings = array('G', 'PG', 'R', 'X');
 	$maxsize = 80;
 	$minsize = 1;
+	$eg_hooks = array(
+	  'get_comment_author_link' => 'Comment Author Link',
+	  'comment_text' => 'Comment Text',
+        );
 	
 	// See if the user has posted us some information
 	// If they did, this hidden field will be set to 'Y'
@@ -114,6 +152,7 @@ function eg_options_page() {
 		$eg_size = (int) $_POST['eg_size'];
 		$eg_defaulturl = $_POST['eg_defaulturl'];
 		$eg_style_span = $_POST['eg_style_span'];
+		$eg_api_hook = $_POST['eg_api_hook'];
 		
 		if ($eg_size > $maxsize) {
 			$eg_size = $maxsize;
@@ -128,6 +167,7 @@ function eg_options_page() {
 		update_option( 'eg_size', $eg_size );
 		update_option( 'eg_defaulturl', $eg_defaulturl );
 		update_option( 'eg_style_span', $eg_style_span );
+		update_option( 'eg_api_hook', $eg_api_hook );
 
 		// Put an options updated message on the screen
 
@@ -143,13 +183,14 @@ function eg_options_page() {
 	$eg_opt_size = $opts['eg_size'];
 	$eg_opt_defaulturl = $opts['eg_defaulturl'];
 	$eg_opt_style_span = $opts['eg_style_span'];
+        $eg_opt_api_hook = $opts['eg_api_hook'];
 	
 // Main options form:
 ?>
 <div class="wrap">
 <h2><?php _e( 'Easy Gravatars Plugin Options', 'eg_trans_domain' ); ?></h2>
 
-<form name="eg_opts" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+<form id="easygravatars" name="eg_opts" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
 <p class="submit">
 <input type="submit" name="Submit" value="<?php _e('Update Options', 'eg_trans_domain' ) ?>" />
 </p>
@@ -157,13 +198,16 @@ function eg_options_page() {
 <?php wp_nonce_field('easy-gravatars-update-options') ?>
 <input type="hidden" name="eg_submitted" value="Y">
 
+<fieldset id="general">
+<legend><?php _e("General Options", 'eg_trans_domain'); ?></legend>
 <p>
-<?php _e("Size, in pixels (maximum: 80):", 'eg_trans_domain' ); ?> 
-<input type="text" name="eg_size" value="<?php echo $eg_opt_size; ?>" size="4">
+<label for="eg_size"><?php _e("Size, in pixels (maximum: 80):", 'eg_trans_domain' ); ?></label>
+<input type="text" name="eg_size" id="eg_size" value="<?php echo $eg_opt_size; ?>" size="4">
 </p>
+
 <p>
-<?php _e("Allowed Rating:", 'eg_trans_domain' ); ?> 
-<select name="eg_rating">
+<label for="eg_rating"><?php _e("Allowed Rating:", 'eg_trans_domain' ); ?></label>
+<select name="eg_rating" id="eg_rating">
 <?php
 	foreach ($eg_ratings as $rating) {
 		$selected = ($rating == $eg_opt_rating) ? 'selected="selected"' : '';
@@ -172,16 +216,33 @@ function eg_options_page() {
 ?>
 </select>
 </p>
+
 <p>
 <div style="float:right;margin-left:10px;width=<?php echo $eg_opt_size ?>"><img src="<?php echo $eg_opt_defaulturl; ?>" width="<?php echo $eg_opt_size; ?>" height="<?php echo $eg_opt_size; ?>" /></div>
-<?php _e("Default image url:", 'eg_trans_domain' ); ?> 
-<input type="text" name="eg_defaulturl" value="<?php echo $eg_opt_defaulturl; ?>" size="40">
+<label for="eg_defaulturl"><?php _e("Default image url:", 'eg_trans_domain' ); ?></label>
+<input type="text" name="eg_defaulturl" id="eg_defaulturl" value="<?php echo $eg_opt_defaulturl; ?>" size="40">
+</p>
+</fieldset>
+
+<fieldset id="advanced">
+<legend><?php _e('Advanced Options', 'eg_trans_domain'); ?></legend>
+<p>
+<label for="eg_style_span"><?php _e("Span style:", 'eg_trans_domain' ); ?></label>
+<input type="text" name="eg_style_span" id="eg_style_span" value="<?php echo $eg_opt_style_span; ?>" size="40">
 </p>
 
 <p>
-<?php _e("Span style:", 'eg_trans_domain' ); ?> 
-<input type="text" name="eg_style_span" value="<?php echo $eg_opt_style_span; ?>" size="40">
+<label for="eg_api_hook"><?php _e("API Hook:", 'eg_trans_domain' ); ?></label>
+<select name="eg_api_hook" id="eg_api_hook">
+<?php
+	foreach ($eg_hooks as $hook => $label) {
+		$selected = ($hook == $eg_opt_api_hook) ? 'selected="selected"' : '';
+		echo "\t<option value='$hook' $selected>$label</option>\n";
+	}
+?>
+</select>
 </p>
+</fieldset>
 
 <p class="submit">
 <input type="submit" name="Submit" value="<?php _e('Update Options', 'eg_trans_domain' ) ?>" />
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: http://dougal.gunters.org/donate
 Tags: comments, gravatars, gravatar, avatars, avatar, images, personalization
 Requires at least: 2.0.4
 Tested up to: 2.3
-Stable tag: 1.0
+Stable tag: 1.1
 
 Add Gravatars to your comments without modifying any template files. Just
 activate, and you're done!
@@ -32,10 +32,53 @@ The plugin will add a new Easy Gravatars section under the Options menu.
 There, you can configure the size and maximum rating of the gravatars that
 you wish to display. You can also set the location of a default image to
 display for users who have no gravatar (the default is a 1px transparent
-gif), and you can tweak the CSS for the wrapper around the image. 
+gif), you can tweak the CSS for the wrapper around the image, and you can
+select to attach the gravatar to either the comment author link, or the text
+of the comment. 
 
 By default, the gravatar is floated to the right of the comment author's
 name, which should work well with most templates.
+
+== Frequently Asked Questions ==
+
+= I have a Gravatar, but it doesn't appear =
+
+The Gravatar service allows you to register multiple email addresses. Make
+sure that the email address you use in your WordPress comments matches an
+email address registered on the Gravatar server.
+
+Also, check the rating of your gravatar versus the maximum allowed rating in
+the Easy Gravatar options. If your gravatar is rated 'R', but you set the
+maximum Easy Gravatar rating to 'PG', the image will be blocked.
+
+= Some people see Gravatars, but others don't =
+
+In some cases, there have been reports that Internet Explorer 6.0 does not
+display gravatars properly. This is probably due to CSS bugs in IE6. I will
+try to isolate this as time allows, but the best advice is for users to
+upgrade to IE7 (for security and better standards support), or to use
+another browser, like Firefox or Opera.
+
+= My comment author links disappear =
+
+You may have another plugin which is conflicting with Easy Gravatars. In
+particular, Joost de Valk's Google Analytics plugin version 1.3 also tried
+to modify the comment author link. Upgrade to version 1.4 or higher of his
+plugin, or select the "Comment Text" API hook in Easy Gravatar's Advanced
+Options.
+
+= Gravatars appear, but... =
+The placement of the Gravatars within your comments template is controlled
+by the `Span style` and `API hook` options. The gravatar is rendered as an
+image tag within a span, which is prepended to either the comment author
+information or the text of the comment, depending on the hook selected.
+
+The default options should work fine with most WordPress templates, but
+there's always the chance that a template with a complex layout might have a
+conflict. If simply changing the API hook selection doesn't give a result
+which is to your liking, you will need to modify the CSS in the span style.
+
+Sorry, but help with CSS styling is beyond the scope of this document. 
 
 == Credits ==
 
